ce609d338bc535dc5699dbcd9da3d982f0a6d933
PRO-AnkitVerma <ankitverma209171@gmail.com>
2020-06-08 09:56:24 +0530

    added module 4 solution

---
+++ module4-soln/solution/script.js
@@ -0,0 +1,14 @@
+(function () { //done
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for(var i = 0; i < names.length; ++i) {
+    if(names[i][0].toLowerCase() === 'j') {
+      byeSpeaker.speak(names[i]);
+    }
+    else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+  
+  })();
